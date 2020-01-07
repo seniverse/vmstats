e6f525fc744952f9c071f2e0e61669516100318f
zhenyi.shu <zhenyi.shu@seniverse.com>
2020-01-07 17:29:35 +0800

    add more metrics

--- src/vmstats_server.erl
+++ src/vmstats_server.erl
@@ -101,6 +101,28 @@ handle_info({timeout, R, ?TIMER_MSG}, S = #state{sink=Sink, key=K, key_separator
     %% Erlang VM uptime stats.
     Sink:collect(timing, [K, "vm_uptime"], erlang:element(1, erlang:statistics(wall_clock))),
 
+    %% Erlang ets count.
+    Sink:collect(gauge, [K, "ets_count"], erlang:system_info(ets_count)),
+
+    %% Erlang thread pool size
+    Sink:collect(gauge, [K, "thread_pool_size"], erlang:system_info(thread_pool_size)),
+
+    Sink:collect(gauge, [K, "dirty_io_schedulers"], erlang:system_info(dirty_io_schedulers)),
+
+    Sink:collect(gauge, [K, "dirty_cpu_schedulers"], erlang:system_info(dirty_cpu_schedulers)),
+
+    Sink:collect(gauge, [K, "dirty_cpu_schedulers_online"], erlang:system_info(dirty_cpu_schedulers_online)),
+
+    Sink:collect(gauge, [K, "schedulers_online"], erlang:system_info(schedulers_online)),
+
+    Sink:collect(gauge, [K, "thread_pool_size"], erlang:system_info(thread_pool_size)),
+
+    {CtxSwitches, RunTime} = erlang:statistics(context_switches),
+    Sink:collect(gauge, [K, "context_switches"], CtxSwitches),
+    Sink:collect(gauge, [K, "runtime"], RunTime),
+
+    Sink:collect(gauge, [K, "active_tasks"], erlang:statistics(total_active_tasks)),
+
     %% Error logger backlog (lower is better)
     case whereis(error_logger) of
         undefined -> ok ;
